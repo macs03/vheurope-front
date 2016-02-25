39c29eab7ede47e0dbdf3b85ff0d6be6c6902daf
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-02-25 11:22:37 -0430

    structure  of application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voyHoyEropeFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the voyHoyEropeFrontApp
+ */
+angular.module('voyHoyEropeFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voyHoyEropeFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voyHoyEropeFrontApp
+ */
+angular.module('voyHoyEropeFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
