c41c97b452976e75df5dd1b46772b49887430488
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-02-26 11:54:34 -0430

    template and controller for search trips

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,9 +20,9 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        templateUrl: 'views/search.tpl.html',
+        controller: 'SearchController',
+        controllerAs: 'search'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
