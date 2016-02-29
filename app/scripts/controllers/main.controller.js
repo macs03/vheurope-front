bbf3963925f5409caf0c0d5dc9f4a5b436dbad22
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-02-29 11:53:33 -0430

    add locations to input's search in main view

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -8,16 +8,12 @@
  * Controller of the vhEurope
  */
 angular.module('vhEurope')
-  .controller('MainController', function ($scope,utilityService) {
+  .controller('MainController', function ($scope,utilityService,locationsFactory) {
+
   	$scope.origin = 0;
   	$scope.destination = 0;
 
-	$scope.myOptions = [
-  		{id: 1, city: 'Barcelona', country: 'España', label: 'Barcelona, España'},
-  		{id: 1, city: 'Madrid', country: 'España', label: 'Madrid, España'},
-      {id: 1, city: 'Bercena', country: 'España', label: 'Barcena, España'},
-
-	];
+	$scope.myOptions = [];
 
 	$scope.myConfig = {
   		create: true,
@@ -38,17 +34,26 @@ angular.module('vhEurope')
         maxDate: moment().add(30, 'days').format('MM-DD-YYYY')
       };
 
+      locationsFactory
+        .getAll()
+        .then(function (data) {
+            $scope.myOptions = data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
 	$scope.searchTrips = function () {
-    	
+
     	console.log($scope.origin);
     	console.log($scope.destination);
     	console.log('departure: '+$scope.dates.departureDate);
-      console.log('returns: '+$scope.dates.returnDate);
-      utilityService.setData($scope.origin, $scope.destination, $scope.dates.departureDate, $scope.dates.returnDate);
-    	
+        console.log('returns: '+$scope.dates.returnDate);
+        utilityService.setData($scope.origin, $scope.destination, $scope.dates.departureDate, $scope.dates.returnDate);
+
 
     };
 
    	$('.header-home.spain').attr('style','background: url("https://dl.dropboxusercontent.com/u/993466/voyhoy/gugenheim.png") no-repeat center center fixed; background-size: cover;');
-  	
+
   });
