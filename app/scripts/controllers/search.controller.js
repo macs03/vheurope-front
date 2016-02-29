489eb92c11a3d255015f7361eb6490f45344549e
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-02-29 13:34:21 -0430

    Form directive in search view, works

--- app/scripts/controllers/search.controller.js
+++ app/scripts/controllers/search.controller.js
@@ -10,49 +10,38 @@
 angular.module('vhEurope')
 
     .controller('SearchController',SearchController);
-    SearchController.$inject =['travelsFactory','utilityService','$scope'];
+    SearchController.$inject =['travelsFactory','utilityService','$scope','$interval'];
 
-    function SearchController (travelsFactory,utilityService,$scope) {
+    function SearchController (travelsFactory,utilityService,$scope,$interval) {
         var vm = this;
         vm.searchTrip = searchTrip;
         vm.searching = false;
         vm.error = false;
         var indexParams = utilityService.getData();
+        var search = $interval(function () {
+            indexParams = utilityService.getData()
+            searchTrip();
+        }, 1000);
 
-        //params from index view
-        if(indexParams.origin){
-            vm.trips = [];
-            vm.searching = true;
-            vm.error = false;
-            travelsFactory
-                .getAll(indexParams.origin,indexParams.destination,indexParams.departure,indexParams.returns,indexParams.passengers)
-                .then((data)=>{
-                    vm.trips = data;
-                    vm.searching = false;
-                })
-                .catch((err)=>{
-                    console.log(err)
-                    vm.searching = false;
-                    vm.error = true;
-                })
-        }
-
-        //params from search view
         function searchTrip() {
-            vm.trips = [];
-            vm.searching = true;
-            vm.error = false;
-            travelsFactory
-                .getAll(vm.origen,vm.destino,vm.ida,vm.vuelta,vm.pasajeros)
-                .then((data)=>{
-                    vm.trips = data;
-                    vm.searching = false;
-                })
-                .catch((err)=>{
-                    console.log(err)
-                    vm.searching = false;
-                    vm.error = true;
-                    vm.msgError = err;
-                })
+            if(indexParams.origin){
+                vm.trips = [];
+                vm.searching = true;
+                vm.error = false;
+                travelsFactory
+                    .getAll(indexParams.origin,indexParams.destination,indexParams.departure,indexParams.returns,indexParams.passengers)
+                    .then((data)=>{
+                        vm.trips = data;
+                        vm.searching = false;
+                    })
+                    .catch((err)=>{
+                        console.log(err);
+                        vm.searching = false;
+                        vm.error = true;
+                        vm.msgError = err;
+                    })
+                utilityService.setData(null,null,null,null);
+                indexParams = null;
+            }
         }
     }
