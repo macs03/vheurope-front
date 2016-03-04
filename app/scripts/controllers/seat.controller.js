63395e1b6dd672a0077b3a4f8b3675cf562d7fc3
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-03-04 08:44:59 -0430

    -. Agregando el modelo a la pagina de asientos

--- app/scripts/controllers/seat.controller.js
+++ app/scripts/controllers/seat.controller.js
@@ -11,9 +11,22 @@ angular
     .module('vhEurope')
     .controller('SeatController',SeatController);
 
-    SearchController.$inject =['travelsFactory','utilityService','$scope','$interval'];
+    SearchController.$inject = ['travelsFactory','utilityService','$scope','$interval'];
 
     function SeatController (travelsFactory, utilityService, $scope, $interval) {
+        
+        var vm = this;
+        vm.trips = {};
+        vm.trips.round = {origin: 'Barcelona', countryOrigin: 'España', countryCodeOrigin: 'es', destination: 'Madrid', countryDestination: 'España', countryCodeDestination: 'es',  departureHour: '12:00', arrivalHour: '23:00', terminalOrigin: 'Terminal Barajas 14C', terminalDestination: 'Terminal Madrid 12C', duration: '10h 23min', company: 'Alsa', companyLogo: 'https://www.voyhoy.com/static/images/60x60/new/bus-logo/chile/expreso-norte.jpg'};
+        vm.trips.return = {origin: 'Madrid', countryOrigin: 'España', countryCodeOrigin: 'es', destination: 'Barcelona', countryDestination: 'España', countryCodeDestination: 'es',  departureHour: '23:00', arrivalHour: '06:00', terminalOrigin: 'Terminal Madrid 12C', terminalDestination: 'Terminal Barajas 14C', duration: '10h 25min', company: 'Alsa', companyLogo: 'https://www.voyhoy.com/static/images/60x60/new/bus-logo/chile/expreso-norte.jpg'};
+
+        vm.seatsSelected = [];
+         
+        console.log(vm);
+
+
+
+    	//Inicializacion de los tabs
         $('#seats-ida a, #seats-vuelta a').click(function (e) {
             e.preventDefault()
             $(this).tab('show')
