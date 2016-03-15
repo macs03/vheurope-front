55fed152f4b3ae382c25a6e909a61e50796792c0
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-03-15 10:15:59 -0430

    -. Agregadas URL como anotación

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,6 +1,8 @@
 //http://vheurope.cfapps.io/v1/
+//http://vheurope-sandbox-env.eu-central-1.elasticbeanstalk.com/v1/
+//http://localhost:8080/vheurope-api/v1/
 (function () {
     'use strict';
     angular.module('constants', [])
-    .constant('apiUrl', 'http://localhost:8080/vheurope-api/v1/');
+    .constant('apiUrl', 'http://vheurope-sandbox-env.eu-central-1.elasticbeanstalk.com/v1/');
 })();
