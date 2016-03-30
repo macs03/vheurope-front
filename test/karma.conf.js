a4dc5326289d9839b6d8d2c3bd114dfbf09e8e89
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-03-30 10:38:44 -0430

    Install and config angular local storage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -44,6 +44,7 @@ module.exports = function(config) {
       'bower_components/angular-loading-bar/build/loading-bar.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-ui-mask/dist/mask.js',
+      'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
