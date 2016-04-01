296f1f4a41840da0b569b6db05c0fb6540df64d4
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-04-01 16:50:32 -0430

    -. Agregada nueva url

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,8 +1,9 @@
 //http://vheurope.cfapps.io/v1/
 //http://vheurope-sandbox-env.eu-central-1.elasticbeanstalk.com/v1/
 //http://localhost:8080/vheurope-api/v1/
+//http://vheurope.eu-central-1.elasticbeanstalk.com/v1/
 (function () {
     'use strict';
     angular.module('constants', [])
-    .constant('apiUrl', 'http://vheurope-sandbox-env.eu-central-1.elasticbeanstalk.com/v1/');
+    .constant('apiUrl', 'http://vheurope.eu-central-1.elasticbeanstalk.com/v1/');
 })();
