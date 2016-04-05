5778ce5941a9364d7c83fd6e542c84de5c068f86
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-04-05 13:29:38 -0430

    -. Agregadas nuevas direcciones

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,9 +1,7 @@
-//http://vheurope.cfapps.io/v1/
-//http://vheurope-sandbox-env.eu-central-1.elasticbeanstalk.com/v1/
-//http://localhost:8080/vheurope-api/v1/
-//http://vheurope.eu-central-1.elasticbeanstalk.com/v1/
+//http://api.resertrip.com/v1/
+//http://sandbox.api.resertrip.com/v1/
 (function () {
     'use strict';
     angular.module('constants', [])
-    .constant('apiUrl', 'http://vheurope.eu-central-1.elasticbeanstalk.com/v1/');
+    .constant('apiUrl', 'http://api.resertrip.com/v1/');
 })();
