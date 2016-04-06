52521409c33693630f0b6921eba42750c93f742b
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-04-06 17:17:17 -0430

    Fixes in hour filter for test

--- app/scripts/filters/hour.filter.js
+++ app/scripts/filters/hour.filter.js
@@ -18,15 +18,16 @@
             if (input.length > 0){
                 var dateUNIX = input[0].departure;
                 var date = new Date(dateUNIX);
-                var day = date.getDate();
+                var day = date.getUTCDate();
                 var month = date.getUTCMonth();
-                var year = date.getFullYear();
+                var year = date.getUTCFullYear();
+                var uso = date.getTimezoneOffset();
                 if (day != 31){
-                    var firstTime = new Date(year,month,day,minValue,0,0);
-                    var secondTime = new Date(year,month,day,maxValue,59,59);
+                    var firstTime = new Date(Date.UTC(year,month,day,minValue,0,0));
+                    var secondTime = new Date(Date.UTC(year,month,day,maxValue,59,59));
                 }else{
-                    var firstTime = new Date(year,month-1,day,minValue,0,0);
-                    var secondTime = new Date(year,month-1,day,maxValue,59,59);
+                    var firstTime = new Date(Date.UTC(year,month-1,day,minValue,0,0));
+                    var secondTime = new Date(Date.UTC(year,month-1,day,maxValue,59,59));
                 }
                 var initialTime = firstTime.getTime();
                 var finalTime = secondTime.getTime();
