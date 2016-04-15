d77bdecc59aabc91b61eaaab3b1825503bef6c0b
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-04-15 13:07:21 -0430

    -. Agregado selector de pasajeros en vista de búsqueda

--- app/scripts/controllers/search.controller.js
+++ app/scripts/controllers/search.controller.js
@@ -399,9 +399,17 @@
             vm.countryDestination = params.countryDestination;
             vm.passengers_options  = ['1', '2', '3', '4', '5'];
             vm.passengers = params.passengers;
+            vm.passengersAdult  = vm.passengers_options [1];
+            vm.passengersChild  = vm.passengers_options [0];
+            vm.passengersBaby  = vm.passengers_options [0];
             vm.weather = weatherFactory.getWeather(params.destination, 'es');
             vm.weather_progressbar.reset();
             vm.weather_progressbar.start();
+
+             vm.selectPassengers = function(){
+                vm.passengers =  parseInt(vm.passengersAdult)  + parseInt(vm.passengersChild) + parseInt(vm.passengersBaby);
+                $('#modal_select_passengers').modal('hide');
+            };
 
             var url = '/search/' + $stateParams.origin + '/' + $stateParams.originCountryCode + '/' + $stateParams.destination + '/' +$stateParams.destinationCountryCode + '/' + $stateParams.departureDate + '/' + $stateParams.returnDate;
             utilityService.setSearch(url);
@@ -1016,6 +1024,10 @@
 
             $('.btn-filters').on('click', function(){
                 $('#filters-container').toggleClass('hidden-xs');
+            });
+
+            $('#select_passengers').on('click', function(){
+                 $('#modal_select_passengers').modal('show');
             });
 
         }
