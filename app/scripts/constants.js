e1c9933a9a344f0e84aec117bf7320a549a491cf
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-04-18 09:50:53 -0430

    -. Cambio menor

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -3,5 +3,5 @@
 (function () {
     'use strict';
     angular.module('constants', [])
-    .constant('apiUrl', 'https://api.resertrip.com/v1/');
+    .constant('apiUrl', 'http://sandbox.api.resertrip.com/v1/');
 })();
