8e616660e09523e45fc448ee21970ddfd5f32114
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-04-22 08:10:28 -0430

    -. Corrección menor

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,5 +1,6 @@
-//http://api.resertrip.com/v1/
+//https://api.resertrip.com/v1/
 //http://sandbox.api.resertrip.com/v1/
+//http://localhost:8080/vheurope-api/v1/
 (function () {
     'use strict';
     angular.module('constants', [])
