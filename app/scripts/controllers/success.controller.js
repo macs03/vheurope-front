d281a287daaaf7c69d24d8bd515755abac1c6722
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-04-28 12:14:11 -0430

    Show data info in success view for mixed trips

--- app/scripts/controllers/success.controller.js
+++ app/scripts/controllers/success.controller.js
@@ -19,10 +19,10 @@ angular
         var successData = sessionStorageService.getSuccessData();
 
         //Eventos Google Analytics
-        $analytics.eventTrack('# Passages Departure', {  category: 'Success', label: 'Number of tickets purchased', value: successData.departureData.seats.length });
-        $analytics.eventTrack('Bus Company Departure', {  category: 'Success', label: successData.departureData.companyName });
-        $analytics.eventTrack('Trip Duration Departure', {  category: 'Success', label: successData.departureData.duration });
-        $analytics.eventTrack('Type Service Departure', {  category: 'Success', label: successData.departureData.typeService });
+        $analytics.eventTrack('# Passages Departure', {  category: 'Success', label: 'Number of tickets purchased', value: successData.departureData[0].seats.length });
+        $analytics.eventTrack('Bus Company Departure', {  category: 'Success', label: successData.departureData[0].companyName });
+        $analytics.eventTrack('Trip Duration Departure', {  category: 'Success', label: successData.departureData[0].duration });
+        $analytics.eventTrack('Type Service Departure', {  category: 'Success', label: successData.departureData[0].typeService });
 
         /*for (var i = 0; i < successData.departureData.seats.length; i++) {
             $analytics.eventTrack('Seat Price Departure', {  category: 'Success', label: 'Price for Seat Number: '+successData.departureData.seats[i].seatNumber, value: successData.departureData.seats[i].price });
@@ -30,10 +30,10 @@ angular
 
         if(typeof(successData.returnData)== 'object'){
 
-            $analytics.eventTrack('# Passages Return', {  category: 'Success', label: 'Number of tickets purchased', value: successData.returnData.seats.length });
-            $analytics.eventTrack('Bus Company Return', {  category: 'Success', label: successData.returnData.companyName });
-            $analytics.eventTrack('Trip Duration Return', {  category: 'Success', label: successData.returnData.duration });
-            $analytics.eventTrack('Type Service Return', {  category: 'Success', label: successData.returnData.typeService });
+            $analytics.eventTrack('# Passages Return', {  category: 'Success', label: 'Number of tickets purchased', value: successData.returnData[0].seats.length });
+            $analytics.eventTrack('Bus Company Return', {  category: 'Success', label: successData.returnData[0].companyName });
+            $analytics.eventTrack('Trip Duration Return', {  category: 'Success', label: successData.returnData[0].duration });
+            $analytics.eventTrack('Type Service Return', {  category: 'Success', label: successData.returnData[0].typeService });
 
             /*for (var i = 0; i < successData.returnData.seats.length; i++) {
                 $analytics.eventTrack('Seat Price Return', {  category: 'Success', label: 'Price for Seat Number: '+successData.returnData.seats[i].seatNumber, value: successData.returnData.seats[i].price });
