969cf6d3d0a8bc5972ef75c373cd0fbfb834bff1
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-05-12 17:04:54 -0430

    -. Cambio menor

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,7 +1,8 @@
+//http://localhost:8080/vheurope-api/v1/
 //http://api.resertrip.com/v1/
 //http://sandbox.api.resertrip.com/v1/
 (function () {
     'use strict';
     angular.module('constants', [])
-    .constant('apiUrl', 'http://sandbox.api.resertrip.com/v1/');
+    .constant('apiUrl', 'http://localhost:8080/vheurope-api/v1/');
 })();
