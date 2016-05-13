40a60148b45d9d927fc5f97f8604a14f668b53ec
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-05-13 16:42:50 -0400

    Get customer data for settings view

--- app/scripts/controllers/customerSettings.controller.js
+++ app/scripts/controllers/customerSettings.controller.js
@@ -12,13 +12,26 @@
         .module('vhEurope')
         .controller('CustomerSettingsController', CustomerSettingsController);
 
-    CustomerSettingsController.$inject = ['$scope', '$location'];
+    CustomerSettingsController.$inject = ['$scope', '$location', 'customerInfoFactory'];
 
-    function CustomerSettingsController($scope, $location) {
+    function CustomerSettingsController($scope, $location, customerInfoFactory) {
         var vm = this;
         var token = localStorage.getItem("resertrip_token");
         if (token == null) {
             $location.path("/login")
+        }else {
+            callCustomerInfo();
+        }
+
+        function callCustomerInfo() {
+            customerInfoFactory
+                .getAll(token)
+                .then(function(data) {
+                    vm.customerData = data;
+                })
+                .catch(function(err) {
+                    console.log(err);
+                })
         }
     }
 
