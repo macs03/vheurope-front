a59203853d34f6dba1c252b0ce05e192236622ad
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-05-13 15:35:44 -0400

    Refactoring code

--- app/scripts/controllers/customerProfile.controller.js
+++ app/scripts/controllers/customerProfile.controller.js
@@ -1,37 +1,37 @@
-(function () {
+(function() {
     'use strict';
 
-/**
- * @ngdoc function
- * @name vhEurope.controller:CustomerProfileController
- * @description
- * # CustomerProfileController
- * Controller of the vhEurope
- */
-angular
-    .module('vhEurope')
-    .controller('CustomerProfileController',CustomerProfileController);
+    /**
+     * @ngdoc function
+     * @name vhEurope.controller:CustomerProfileController
+     * @description
+     * # CustomerProfileController
+     * Controller of the vhEurope
+     */
+    angular
+        .module('vhEurope')
+        .controller('CustomerProfileController', CustomerProfileController);
 
     CustomerProfileController.$inject = ['$scope', '$location', 'customerInfoFactory'];
 
-    function CustomerProfileController ($scope, $location, customerInfoFactory) {
+    function CustomerProfileController($scope, $location, customerInfoFactory) {
         var vm = this;
         vm.callCustomerInfo = callCustomerInfo;
         vm.customerData = null;
         var token = localStorage.getItem("resertrip_token");
         if (token == null) {
             $location.path("/login")
-        }else{
+        } else {
             callCustomerInfo();
         }
 
         function callCustomerInfo() {
             customerInfoFactory
                 .getAll(token)
-                .then(function (data) {
+                .then(function(data) {
                     vm.customerData = data;
                 })
-                .catch(function (err) {
+                .catch(function(err) {
                     console.log(err);
                 })
         }
