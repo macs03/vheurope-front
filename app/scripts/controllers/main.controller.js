5d1dfe660ea57f99e433ed4971ea67b92ceb797a
Jesus Colmenares <jesuscolorozco@gmail.com>
2016-05-24 13:01:12 -0400

    -. Corrección menor

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -149,7 +149,7 @@
                 preload: true,
                 load: function(query, callback) {
                     if (!query.length) return callback();
-                    if (query.length >= 3){
+                   // if (query.length >= 3){
                         vm.myOptionsOrigin = [];
                         locationsRtFactory
                         .getAll(query)
@@ -163,7 +163,7 @@
                         .catch(function (err) {
                              callback();
                         });
-                    }
+                    //}
                 }
         	};
 
