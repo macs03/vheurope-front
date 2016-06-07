f50dfc28a41e53bba510323076ea26a94659a1ff
Abel Rangel <ransilad@gmail.com>
2016-06-07 15:29:02 -0400

    Inicializar el filtro del precio y variable auxPrice

--- app/scripts/controllers/search.controller.js
+++ app/scripts/controllers/search.controller.js
@@ -932,6 +932,19 @@
                 vm.isLoading = true;
                 vm.countBusSearch = 0;
                 vm.minDuration = 0;
+                vm.priceSlider = {
+                   price: 0,
+                   options: {
+                       showSelectionBar: true,
+                       translate: function(value) {
+                           return '€' + value;
+                       },
+                       floor: 0,
+                       ceil: 0,
+                   }
+                };
+                auxPrice = 0;
+
             };
 
             var loadGlobal = function(data, isMixed, isPlane){
