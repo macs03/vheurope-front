d9a24aee620d0532122fdbd93b29a5d54ce0b1e1
Abel Rangel <ransilad@gmail.com>
2016-06-08 11:19:30 -0400

    Revert "Corrección para cuando selecciono Combinar y aplico filtros se quitan los iconos del tipo de viaje"
    This reverts commit 914f8a9d3755de8978cb40fcaefa49ab0de135ba.

--- app/scripts/controllers/search.controller.js
+++ app/scripts/controllers/search.controller.js
@@ -2366,7 +2366,6 @@
 
             function processCountOrder () {
                   $('[data-toggle="tooltip"]').tooltip();
-                  $('.fa-trip-type').addClass('hidden');
                   vm.countOrder = vm.countOrder + 1;
                   if (vm.countOrder == 4) {
                         order('departure');
