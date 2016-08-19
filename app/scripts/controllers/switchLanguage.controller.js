80ee48d60da188aca0e057288ddc5173b4877557
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-08-19 12:09:00 -0400

    add options to change country

--- app/scripts/controllers/switchLanguage.controller.js
+++ app/scripts/controllers/switchLanguage.controller.js
@@ -17,6 +17,12 @@
     function switchLanguageController($scope, $translate, $cookieStore, utilityService, sessionStorageService, $rootScope) {
 
         $scope.selectedLanguage = $cookieStore.get('NG_TRANSLATE_LANG_KEY');
+        $scope.selectedCountry = utilityService.getCountry()
+
+        $scope.changeCountry = function(country) {
+            console.log('cambiando pais ' + country);
+        }
+
         $scope.changeLanguage = function(langKey) {
             $translate.use(langKey);
             $scope.selectedLanguage = langKey;
